d69a78208741b9a71545e0da8c879f49a749563f
Sodiq Alabi <adisco4420@gmail.com>
2021-03-14 10:03:49 +0100

    home component

---
+++ src/app/layouts/main/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(public sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.sliderInit();
+  }
+
+  sliderInit = () => {
+    const glider = (window as any).Glider;
+    const d = new glider(document.querySelector('.glider'), {
+      slidesToShow: 3,
+      dots: '#dots',
+      draggable: true,
+      arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+      }
+    });
+    return d;
+  }
+
+}
