680363322c160e80068afb8686b313af666e7277
Sodiq Alabi <adisco4420@gmail.com>
2021-03-14 10:01:06 +0100

    route to main module & auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./layouts/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./layouts/auth/auth.module').then(m => m.AuthModule)
+  },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
