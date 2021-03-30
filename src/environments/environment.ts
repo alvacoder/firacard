a0a5328369a69b491a2a97fa6ec52253a80c6b20
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2021-03-30 18:54:53 +0100

    change google and fb app id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   apiUrl: 'https://firacard-express.herokuapp.com/v1',
   google: {
-    clientId: '88594060065-qnjot70qi25793smlmotomvid5me30h9.apps.googleusercontent.com',
+    clientId: '1078296083159-cafim0io06hqo4l97qb440r0c4frv5qn.apps.googleusercontent.com',
   },
   facebook: {
-    appId: '315520406177170'
+    appId: '963529757518264'
   }
 };
 
