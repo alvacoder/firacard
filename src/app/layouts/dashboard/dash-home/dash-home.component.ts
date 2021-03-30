235322a15610888f7e236db503d3bfcc649049b3
Sodiq Alabi <adisco4420@gmail.com>
2021-03-30 18:56:24 +0100

    create & get boards

--- src/app/layouts/dashboard/dash-home/dash-home.component.ts
+++ src/app/layouts/dashboard/dash-home/dash-home.component.ts
@@ -1,6 +1,8 @@
 import { BoardService } from './../services/board.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dash-home',
@@ -8,11 +10,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./dash-home.component.scss']
 })
 export class DashHomeComponent implements OnInit {
-  cards = [1, 2, 3, 4];
+  boards = [];
   activeTab = 'curratedCards';
+  form = this.fb.group({
+    boardTitle: ['', [Validators.required]],
+    recipients: this.fb.array([this.createRecp()])
+  });
+  loading = false;
 
+  get recpForm(): FormArray {return this.form.get('recipients') as FormArray; }
   constructor(
     private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private toastr: ToastrService,
     private boardSrv: BoardService) { }
 
   ngOnInit(): void {
@@ -23,8 +33,33 @@ export class DashHomeComponent implements OnInit {
   }
   getBoards(): void {
     this.boardSrv.getUserBoards().subscribe(res => {
+      this.boards = res.payload;
       console.log(res);
     });
   }
+  get getRecpControls(): any[] {
+    return ((this.form.get('recipients') as any).controls);
+  }
+  createRecp(): FormGroup {
+    return this.fb.group({
+      name: ['']
+    });
+  }
+  addRecp(): void {
+    this.recpForm.push(this.createRecp());
+  }
+  removeRecp(i: any): void {
+    this.recpForm.removeAt(i);
+  }
 
+  createBoard(): void {
+    if (this.form.valid) {
+      const payload = {boardTitle: this.form.value.boardTitle};
+      this.loading = true;
+      this.boardSrv.createBoard(payload).subscribe(res => {
+        this.toastr.success('Board created successfully');
+        this.getBoards();
+      }).add(() => this.loading = false);
+    }
+  }
 }
