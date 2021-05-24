26c5be691d0a12fae2c5e6dcb1aea53a8d2cd312
alvacoder <idrisadeniji01@gmail.com>
2021-05-24 00:11:40 -0400

    switch to firacard fb app id on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     clientId: '88594060065-qnjot70qi25793smlmotomvid5me30h9.apps.googleusercontent.com',
   },
   facebook: {
-    appId: '313244885878489'
+    appId: '963529757518264'
   },
   stripePublicKey: 'pk_test_51IcYm2BC0cB59uLZ5Qo5fc4JT8c70UBO0klCxZTjTsYEWJHGuZ0fb8U610QQ9bl6GGkFAouOD4sGHkrbM7VedsBU00jG4wq0jk',
   cloudinary: {apiKey: '247151848688876', cloudName: 'dx5bcp5ps', secret: ''}
